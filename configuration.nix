75477d3483fb92a1076e1a71d68d985e41dec62e
emilyrhee <emilyrhee99@gmail.com>
2024-08-27 13:21:29 -0400

    added user emilyrhee to dialout group
    for uploading Arduino sketches without chmoding everytim

--- configuration.nix
+++ configuration.nix
@@ -88,6 +88,10 @@
     source = "${pkgs.util-linux}/bin/write";
   };
 
+  users.users.emilyrhee = {
+    extraGroups = [ "dialout" ];
+  };
+
   # Some programs need SUID wrappers, can be configured further or are started in user sessions. programs.mtr.enable = true; 
   # programs.gnupg.agent = {
   #   enable = true; enableSSHSupport = true;
